a13820c11d8ac87adb6d188ecbcefe3088a94966
Marcin Wolder <marcinwolder7@gmail.com>
2025-06-08 00:18:04 +0200

    new recurency

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import threading
 from antlr4 import CommonTokenStream, FileStream
 from ErrorListener import ErrorListener
 from ScriptoriumVisitor import Visitor
@@ -7,6 +8,7 @@ from Scriptorium.ScriptoriumLexer import ScriptoriumLexer
 from Scriptorium.ScriptoriumParser import ScriptoriumParser
 
 def main():
+    sys.setrecursionlimit(100000)
     var_map = {}
 
     input_stream = FileStream(sys.argv[1], encoding='utf-8')
@@ -27,7 +29,7 @@ def main():
         parser.addParseListener(VariableListener(var_map))
         tree = parser.start()
 
-        if sys.argv[2] == "tree":
+        if len(sys.argv) >= 3 and sys.argv[2] == "tree":
             from dot_tree import tree_to_dot
             tree_to_dot(tree, parser, outfile="tree.dot", to_png=True)
             print(var_map)
@@ -40,7 +42,15 @@ def main():
 
         # print(3)
     except Exception as e:
-        print(e)
+        if str(e).startswith("CULPA"):
+            print(e)
+        else:
+            raise e
 
 if __name__ == "__main__":
-    main()
+    threading.stack_size(32 * 1024 * 1024)
+
+    t = threading.Thread(target=main)
+    t.daemon = True
+    t.start()
+    t.join()
